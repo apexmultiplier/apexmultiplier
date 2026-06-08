642514780aaab829c6a9ef52a993a7d5f9df6cc8
Aakif <apexmultiplier@gmail.com>
2026-06-08 17:09:53 +0530

    Fix bonuses page useEffect cleanup

--- app/admin/bonuses/page.tsx
+++ app/admin/bonuses/page.tsx
@@ -58,7 +58,9 @@ export default function BonusesPage() {
       )
       .subscribe()
 
-    return () => channel.unsubscribe()
+    return () => {
+      void supabase.removeChannel(channel)
+    }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
